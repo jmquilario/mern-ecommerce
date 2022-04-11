736bc0cdb8d2d93c5255410a039016390f4c8725
John Marvin Quilario <jmquilario@gmail.com>
2022-04-11 09:50:06 +0800

    Setup Data Seeder Script for Populating Database for Testing

---
+++ backend/seeder.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import color from 'colors'
+import users from './data/users.js'
+import products from './data/products.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
+import { object } from 'webidl-conversions'
+
+dotenv.config()
+
+connectDB()
+
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    const createdUser = await User.insertMany(users)
+
+    const adminUser = createdUser[0]._id
+
+    const sampleProducts = products.map((product) => {
+      return {
+        ...product,
+        user: adminUser,
+      }
+    })
+
+    await Product.insertMany(sampleProducts)
+
+    console.log('Data Imported!'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    console.log('Data Destroyed!'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+
+/*
+    process.argv[0] == "path/to/node"
+    process.argv[1] == "path/to/myprogram.js"
+    process.argv[2] == "firstarg"
+    process.argv[3] == "secondarg"
+    The process. argv property is an inbuilt application programming interface of the process module 
+    which is used to get the arguments passed to the node. js process when run in the command line
+*/
+
+process.argv[2] === '-d' ? destroyData() : importData()
