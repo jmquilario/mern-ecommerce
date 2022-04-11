42630e23b40d1d74d5d898158fca5ed5f2c87c50
John Marvin Quilario <jmquilario@gmail.com>
2022-04-11 18:30:57 +0800

    Added Message & Loader Components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 /*
 Axios is a promise based HTTP client for the browser and Node. js. 
@@ -14,9 +16,12 @@ const HomeScreen = () => {
   const dispatch = useDispatch()
 
   // From productReducer
+  // useSelector grab it from the state
   const productList = useSelector((state) => state.productList)
   const { loading, error, products } = productList
 
+  // fire the function from productActions
+  // send it to the reducer
   useEffect(() => {
     dispatch(listProducts())
   }, [dispatch])
@@ -25,9 +30,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Product</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
