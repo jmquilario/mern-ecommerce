3771d03102a1e35c36e2c5b0c52b86956351f3d4
John Marvin Quilario <jmquilario@gmail.com>
2022-04-11 18:14:03 +0800

    Using Redux State into HomeScreen - useDispatch & useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,38 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
 /*
 Axios is a promise based HTTP client for the browser and Node. js. 
 Axios makes it easy to send asynchronous HTTP requests to REST endpoints and perform CRUD operations. 
 It can be used in plain JavaScript or with a library such as Vue or React
 */
-import axios from 'axios'
+// import axios from 'axios'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  // From productReducer
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      // below end point will return data
-      // destructure to directly access it
-      const { data } = await axios.get('/api/products')
-
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1>Latest Product</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
