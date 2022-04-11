42b70f728694381240f5fd6d58f90dd74ea00389
John Marvin Quilario <jmquilario@gmail.com>
2022-04-11 15:45:22 +0800

    Added Custom Error Handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -23,9 +23,13 @@ router.get(
     //const product = products.find((p) => p._id === req.params.id)
     const product = await Product.findById(req.params.id)
 
-    product
-      ? res.json(product)
-      : res.status(404).json({ message: 'Product not found' })
+    // Error Handler
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
   })
 )
 
