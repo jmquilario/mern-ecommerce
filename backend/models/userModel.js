8a8c72e98ecc4b51414f234631233ada95951ad2
John Marvin Quilario <jmquilario@gmail.com>
2022-04-11 08:48:45 +0800

    Added Models

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: [true, 'isAdmin is required'],
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
