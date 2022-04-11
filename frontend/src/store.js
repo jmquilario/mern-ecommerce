952c2154420dd08a5ac6fb86b4104dd9ca9a8b5b
John Marvin Quilario <jmquilario@gmail.com>
2022-04-11 19:31:46 +0800

    Added Product Details Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,16 @@
+// Everytime there is a new reducer created, add it always here
+
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 
 const initialState = {}
